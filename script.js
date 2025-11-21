d51adb1dbdeb105aa0d5cc55267ddfb2e3fc9eb2
rahulraj9690 <168002893+rahulraj9690@users.noreply.github.com>
2025-11-21 22:52:48 +0530

    Refactor music volume settings on page load
    Removed duplicate event listeners and adjusted music volume.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Website Loaded Successfully.");
 
 document.addEventListener("DOMContentLoaded", () => {
     const music = document.getElementById("bg-music");
-    music.volume = 0.4;  // soft volume
+    music.volume = 0.5;  // soft volume
 });
 
 
@@ -11,4 +11,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const music = document.getElementById("bg-music");
     music.volume = 0.2;  // soft volume
 });
+
 
