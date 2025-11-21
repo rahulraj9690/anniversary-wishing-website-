6178f2e5e43075812f7bea04f220e5cb163d8af4
rahulraj9690 <168002893+rahulraj9690@users.noreply.github.com>
2025-11-21 22:51:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Website Loaded Successfully.");
 
 document.addEventListener("DOMContentLoaded", () => {
     const music = document.getElementById("bg-music");
-    music.volume = 0.2;  // soft volume
+    music.volume = 0.4;  // soft volume
 });
 
 
@@ -11,3 +11,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const music = document.getElementById("bg-music");
     music.volume = 0.2;  // soft volume
 });
+
